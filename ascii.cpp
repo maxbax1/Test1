db059999e6021b0fd53beb61fe90d99a83774c08
maxbax1 <31599249+maxbax1@users.noreply.github.com>
2017-09-18 19:56:20 +0300

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,15 @@
+// ascii.cpp: ASCII table
+
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	for (int i = 0; i < 256; i++) 
+		cout << char(i)<<"\n";
+
+			return 0;
+	
+
+}
